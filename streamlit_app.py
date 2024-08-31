32d565f925086df743cf9d8df0bfd7642f9813a7
borom <nico.aragonbarrero@gmail.com>
2024-08-31 17:14:43 +0200

    committing from Surface borom

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 #st.title("Welcome to Proc Doc")
 
 # Read the HTML file
-with open("templates/index.html", 'r', encoding='utf-8') as html_file:
+with open("index.html", 'r', encoding='utf-8') as html_file:
     html_content = html_file.read()
 
 # Render the HTML file in Streamlit
