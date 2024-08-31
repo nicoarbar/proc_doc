75cff33a8123bde66587367e149796a82e290ce6
borom <nico.aragonbarrero@gmail.com>
2024-08-31 18:49:37 +0200

    committing from Surface borom

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,20 @@
-import streamlit as st 
-import streamlit.components.v1 as components
+import streamlit as st
 
-#st.title("Welcome to Proc Doc")
-
-# Read the HTML file
-with open("index.html", 'r', encoding='utf-8') as html_file:
-    html_content = html_file.read()
-
-# Render the HTML file in Streamlit
 st.title("Welcome to Proc Doc")
-components.html(html_content, height=1600)
+
+# Just add it after st.sidebar:
+a = st.sidebar.radio('Choose:',[1,2])
+
+
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see #*
+st.caption('Balloons. Hundreds of them...')
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
+
+# * optional kwarg unsafe_allow_html = True
