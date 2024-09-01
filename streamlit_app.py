00219cb38e1c4cfbc65995022eca6543fbd36a52
borom <nico.aragonbarrero@gmail.com>
2024-09-01 02:56:26 +0200

    updating UI completeley

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,41 @@
 import streamlit as st
 
-sign = 'Building DataOps'
-st.title("Welcome to Proc Doc")
-st.header(sign)
-st.image('static/nico_photo.jpg')
-st.subheader(sign)
-st.text(sign)
-st.write(sign)
-st.caption(sign)
+#Title
+st.title("Proc Doc")
+st.subheader("Welcome to automatic document processing")
+st.caption('Proc Doc allows you to input documents and automate the process of formatting them with parameters')
 
-st.button('Hit me')
-data=3
-#st.data_editor('Edit data', data)
-st.checkbox('Check me out')
-a= st.radio('Pick one:', ['nose','ear'])
-st.subheader(a)
-st.selectbox('Select', [1,2,3])
-st.multiselect('Multiselect', [1,2,3])
-st.slider('Slide me', min_value=0, max_value=10)
-st.select_slider('Slide to select', options=[1,'2'])
-st.text_input('Enter some text')
-st.number_input('Enter a number')
-st.text_area('Area for textual entry')
-st.date_input('Date input')
-st.time_input('Time entry')
-st.file_uploader('File uploader')
-#st.download_button('On the dl', data)
-#st.camera_input("一二三,茄子!")
-st.color_picker('Pick a color')
+# Selection of inputs
+st.info('What do you want to do?')
+action = st.radio('Select your action:', 
+                  ['Format docs with input parameters', 
+                  'Ask GPT to verify statements', 
+                  'Indexing documents for information retrieval'])
 
-st.image('static/favicon2.png')
+#Actions
+if action == 'Ask GPT to verify statements':
+    st.error('Feature under construction')
+
+elif action == 'Indexing documents for information retrieval':
+    st.error('Feature under construction')
+
+elif action == 'Format docs with input parameters':
+    st.file_uploader('File uploader')
+    st.success('File processed correctly')
 
 
-# Use widgets' returned values in variables
-#for i in range(int(st.number_input('Num:'))): 
-#    foo()
-#if st.sidebar.selectbox('I:',['f']) == 'f': b()
-#my_slider_val = st.slider('Quinn Mallory', 1, 88)
-#st.write(slider_val)
+    st.button('Hit me')
+    #st.data_editor('Edit data', data)
+    st.multiselect('Multiselect', [1,2,3])
+    st.text_input('Enter some text')
+    st.date_input('Date input')
 
-# Insert containers separated into tabs:
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
-
-# You can also use "with" notation:
+#Ending
+tab1, tab2 = st.tabs(["Pricing", "About"])
 with tab1:
-  st.radio('Select one:', [1, 2])
+    st.warning("This is an Open Source project, however you are welcome to contribute with donations as a Github Sponsor here:")
+    st.write("https://github.com/nicoarbar/proc_doc")
+with tab2:
+    st.warning("""Proc Doc is under construction.
+              For any inquiries, changes and requests please reach out here:""")
+    tab2.write("https://nicoarbar.github.io/proc_doc/")  
