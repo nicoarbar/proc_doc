2bf80c903c064be238a9a07a022cf7c8cc71442c
borom <nico.aragonbarrero@gmail.com>
2024-09-08 14:42:04 +0200

    committing from Surface borom

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
-streamlit run streamlit_app.py
-#start http://localhost:8501
-#docker build -t streamlit-app .
-#docker run -p 8501:8501 streamlit-app
+#!/bin/bash
 
-#FLASK
-#cd..
-#python flask_app.py
-#start http://localhost:5000
+if [ "$1" == "push" ]; then
+    git add .
+    git commit -m "committing from Surface borom"
+    git push
+else
+    streamlit run streamlit_app.py
+    #start http://localhost:8501
+    #docker build -t streamlit-app .
+    #docker run -p 8501:8501 streamlit-app
+
+    #FLASK
+    #cd..
+    #python flask_app.py
+    #start http://localhost:5000
+fi
